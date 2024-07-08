6dec6dfd70649520e46ca4cfb77126b035922f03
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-07-07 21:34:40 -0300

    Decentralized Event Manager
    Desc.:
    
    -Decentralized Event Manager on Solana env with Rust Lang.

---
+++ programs/event-manager/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5sgih4PHTwu1sqxK6Lmr8ZjA9WAHMjev1QMLVychFvrB");
+
+#[program]
+pub mod event_manager {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
