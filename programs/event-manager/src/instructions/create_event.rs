87069ae3d5e98b0be27a514adf4dad3c41617c86
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-07-07 22:54:18 -0300

    Create Event function
    Act.:
    
    -src/lib.rs

--- programs/event-manager/src/instructions/create_event.rs
+++ programs/event-manager/src/instructions/create_event.rs
@@ -65,3 +65,18 @@ pub struct CreateEvent<'info> {
     pub token_program: Program<'info, Token>, // token mint & token acounts
     pub system_program: Program<'info, System>, // accounts created
 }
+
+pub fn handle(ctx: Context<CreateEvent>, name: String, ticket_price: u64) -> Result<()> {
+    ctx.accounts.event.name = name;
+    ctx.accounts.event.ticket_price = ticket_price;
+    ctx.accounts.event.active = true;
+
+    ctx.accounts.event.authority = ctx.accounts.authority.key();
+    ctx.accounts.event.accepted_mint = ctx.accounts.accepted_mint - key();
+
+    ctx.accounts.event.event_bump = ctx.bumps.event;
+    ctx.accounts.event.event_mint_bump = ctx.bumps.event_mint;
+    ctx.accounts.event.treasury_vault_bump = ctx.bumps.treasury_vault;
+    ctx.accounts.event.gain_vault_bump = ctx.bumps.gain_vault;
+    Ok(())
+}
--- programs/event-manager/src/lib.rs
+++ programs/event-manager/src/lib.rs
@@ -1,4 +1,8 @@
-use anchor_lang::prelude::*;
+use {crate::instructions::*, anchor_lang::prelude::*};
+
+
+mod collections;
+mod instructions;
 
 declare_id!("5sgih4PHTwu1sqxK6Lmr8ZjA9WAHMjev1QMLVychFvrB");
 
@@ -6,10 +10,13 @@ declare_id!("5sgih4PHTwu1sqxK6Lmr8ZjA9WAHMjev1QMLVychFvrB");
 pub mod event_manager {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn create_event(
+        ctx: Context<CreateEvent>,
+        name: String,
+        ticket_price: u64,
+    ) -> Result<()> {
+        instructions::create_event::handle(ctx, name, ticket_price)
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize {}
+    
+}
