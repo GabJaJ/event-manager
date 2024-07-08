9a3194af05a9ef79729c32aa08e119c6e967b882
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-07-07 21:49:55 -0300

    Event struct
    Act.:
    
    -src/collections/event.rs
    
    Obj.:
    
    -Event structure.

---
+++ programs/event-manager/src/collections/event.rs
@@ -0,0 +1,28 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(initSpace)] //automatic space calc
+pub struct Event {
+    // event data
+    #[max_len(40)]
+    pub name: String,
+    pub ticket_price: u64,
+    pub active: bool,
+
+    // event accounts
+    pub authority: Pubkey,
+    pub accepted_mint: Pubkey,
+
+    // PDAs bumps
+    pub event_bump: u8,
+    pub event_mint_bump: u8,
+    pub event_vault_bump: u8,
+    pub gain_vault_bump: u8,
+}
+
+impl Event {
+    pub const SEED_EVENT: &'static str = "event";
+    pub const SEED_EVENT_MINT: &'static str = "event_mint";
+    pub const SEED_TREASURY_VAYLT: &'static str = "treasury_vault";
+    pub const SEED_GAIN_VAULT: &'static str = "gain_vault";
+}
