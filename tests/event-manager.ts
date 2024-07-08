ea5a722f910f231d86ddc1782b455aeafc913649
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-07-07 23:17:47 -0300

    Test Event Manager Config
    Act.:
    
    -tests/event-manager.ts

--- tests/event-manager.ts
+++ tests/event-manager.ts
@@ -1,12 +1,28 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { EventManager } from "../target/types/event_manager";
+import { BN } from "bn.js";
+import { Keypair, PublicKey } from '@solana/web3.js';
 
 describe("event-manager", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.EventManager as Program<EventManager>;
+
+  // event test data
+  const name: string = "my_event";
+  const ticketPrice = new BN(1);
+
+  // event accounts adress
+  let acceptedMint: PublicKey;
+
+  // PDAs
+  let eventPublicKey: PublicKey;
+  let eventMint: PublicKey;
+  let treasuryVault: PublicKey;
+  let gainVault: PublicKey;
 
   it("Is initialized!", async () => {
     // Add your test here.
