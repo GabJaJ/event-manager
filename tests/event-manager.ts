c633fd7d499dcb073ed43b6dbfb2ce58066b818c
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-07-09 19:50:28 -0300

    event-manager.ts updated
    Act.:
    
    -tests/event-manager.ts

--- tests/event-manager.ts
+++ tests/event-manager.ts
@@ -3,7 +3,8 @@ import { Program } from "@coral-xyz/anchor";
 import { EventManager } from "../target/types/event_manager";
 import { BN } from "bn.js";
 import { Keypair, PublicKey } from '@solana/web3.js';
-import { createFundedWallet, createMint } from './utils';
+import { createFundedWallet, createMint, createAssociatedTokenAccount } from './utils';
+import { getAssociatedTokenAddress, getAccount } from '@solana/spl-token';
 
 describe("event-manager", () => {
   // Configure the client to use the local cluster.
@@ -61,7 +62,7 @@ describe("event-manager", () => {
     alice = await createFundedWallet(provider, 3);
     // create alice accepted mint ata with 100 accepted mint
     // Accepted mint = USDC  -> alice wallet = 100 USDC 
-    aliceAcceptedMintATA = await createAssociatedTokenAccount(provider,acceptedMint,100, alice);
+    aliceAcceptedMintATA = await createAssociatedTokenAccount(provider, acceptedMint, 100, alice);
     // find alice event mint ata (only finds address)
     aliceEventMintATA = await getAssociatedTokenAddress(eventMint, alice.publicKey);
 
@@ -90,4 +91,45 @@ describe("event-manager", () => {
     const eventAccount = await program.account.event.fetch(eventPublicKey);
     console.log("Event info: ", eventAccount);
   });
-});
+
+  // TEST: Sponsor event
+  it("Alice Should get 5 event tokens", async () => {
+    // show alice accepted mint (USDC) ATA info
+    // should have 100 USDC
+    let aliceUSDCBalance = await getAccount(
+      provider.connection,
+      aliceAcceptedMintATA // Alice Accepted mint account (USDC account)
+    );
+    console.log("Alice Accepted mint ATA: ", aliceUSDCBalance);
+
+    const quantity = new BN(5); // 5 USDC 
+    await program.methods
+      .sponsorEvent(quantity)
+      .accounts({
+        eventMint: eventMint, // 1:1 with USDC
+        payerAcceptedMintAta: aliceAcceptedMintATA, // Alice USDC Account 
+        event: eventPublicKey,
+        authority: alice.publicKey,
+        payerEventMintAta: aliceEventMintATA, // Alice Event Mint Account
+        treasuryVault: treasuryVault // store all Accepted mint (USDC) from sponsorships
+      })
+      .signers([alice])
+      .rpc();
+    // show alice event mint ATA info
+    // should have 5 Event mint
+    const aliceAccount = await getAccount(
+      provider.connection,
+      aliceEventMintATA // Alice Event Mint account (should have <quantity> tokens from sponsorship)
+    );
+    console.log("Alice Event mint ATA: ", aliceAccount);
+
+    // show alice accepted mint (USDC) ATA info
+    // should have 95 (100-5) USDC
+    aliceUSDCBalance = await getAccount(
+      provider.connection,
+      aliceAcceptedMintATA // Alice Accepted mint account (USDC account)
+    );
+    console.log("Alice Accepted mint ATA: ", aliceUSDCBalance);
+
+    
+  });
