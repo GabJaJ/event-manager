20148f30aa8d86507b64eca77ababd21ff94adfc
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-07-28 21:39:34 -0300

    close event added
    Added:
    
    -src/instructions/close_event.rs
    
    Act.:
    
    -src/instructions/mod.rs

--- programs/event-manager/src/instructions/mod.rs
+++ programs/event-manager/src/instructions/mod.rs
@@ -1,7 +1,12 @@
 pub mod buy_tickets;
 pub mod create_event;
 pub mod sponsor;
+pub mod withdraw_funds;
+pub mod close_event;
 
 pub use buy_tickets::*;
 pub use create_event::*;
 pub use sponsor::*;
+pub use withdraw_funds::*;
+pub use close_event::*;
+
